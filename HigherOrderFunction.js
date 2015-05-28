3531cbbbc1383c956291b2e19e4db9d5b05a950c
Guanglin Du <guanglin.du@gmail.com>
2015-05-28 16:26:09 +0800

    Update HigherOrderFunction.js

--- HigherOrderFunction.js
+++ HigherOrderFunction.js
@@ -1,7 +1,13 @@
-// Create on May 27, 2015 Wed.
-// Page 86, 5 Higher-Order Functions, Eloquent JavaScript, A Modern Introduction to Programming by Marijn Haverbeke
-// Run with Node.js: node ./HigherOrderFuncton.js
-
+/**
+ * Create on May 27, 2015 Wed.
+ * Page 86, 5 Higher-Order Functions, Eloquent JavaScript, A Modern Introduction to Programming by Marijn Haverbeke
+ * See also 'Functions as values' on page 46.
+ * Run with Node.js: node ./HigherOrderFuncton.js
+ * 
+ * Higher-order functions:
+ * Functions that operate on other functions, either by taking them as
+ * arguments or by returning them, are called higher-order functions.
+ */
 var total = 0, count = 1;
 while (count <= 10) {
   total += count;
@@ -30,17 +36,43 @@ function logEach(array) {
 console.log("--- Passes our action as a function value ---");
 function forEach2(array, action) {
   for (var i = 0; i < array.length; i++)
-   action(array[i]);
+    action(array[i]);
 }
-forEach2(["Wampeter" , "Foma" , "Granfalloon"] , console.log);
+forEach2(["Wampeter", "Foma", "Granfalloon"], console.log);
 
 /**
  * Often, you don’t pass a predefined function to forEach
  * but create a function value on the spot instead.
  */
-console.log("--- Creates a function value on the spot  ---"); 
+console.log("--- Creates a function value on the spot ---"); 
 var numbers = [1, 2, 3, 4, 5], sum = 0;
 forEach2(numbers, function(number) {
   sum += number;
 });
 console.log(sum); // → 15
+
+// Two array-traversing loops
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
+
+// Working with forEach makes it slightly shorter and quite a bit cleaner.
+function gatherCorrelations2(journal) {
+  var phis = {};
+  journal.forEach(function(entry) {
+    entry.events.forEach(function(event) {
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    });
+  });
+  return phis;
+}
