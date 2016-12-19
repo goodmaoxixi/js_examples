1320192fcc6a905e334ba5e1c97c849452dcdc89
Guanglin Du <guanglin.du@gmail.com>
2016-12-19 09:41:02 +0800

    Modify jquery/method-data.js

--- jquery/method-data.js
+++ jquery/method-data.js
@@ -1,6 +1,16 @@
 // http://api.jquery.com/data/
 $(function(){
   $("#test_method_data").on("click", test);
+  
+  // http://edgeguides.rubyonrails.org/working_with_javascript_in_rails.html
+  $("a[data-background-color]").click(function(e) { // OK
+  //$("a[data-background-color]").on("click", function(e) { // OK
+    var backgroundColor, textColor;
+    e.preventDefault();
+    backgroundColor = $(this).data("background-color");
+    textColor = $(this).data("text-color");
+    return paintIt(this, backgroundColor, textColor);
+  });
 });
 
 var test = function() {
@@ -18,4 +28,9 @@ var test = function() {
   console.log("source: ", $("#test_method_data").data("source"));
   console.log("array: ", $("#test_method_data").data("array"));
   console.log("object: ", $("#test_method_data").data("object"));
+};
+
+var paintIt = function(element, backgroundColor, textColor) {
+  element.style.backgroundColor = backgroundColor;
+  if (textColor != null) return element.style.color = textColor;
 };
