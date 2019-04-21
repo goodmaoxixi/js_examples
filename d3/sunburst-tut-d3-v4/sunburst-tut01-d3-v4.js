4e1585df521b0d128af82fcab381c63545203899
Guanglin Du <guanglin.du@gmail.com>
2019-04-21 22:17:42 +0800

    Continue with sunburst-tut01-d3-v4.js

--- d3/sunburst-tut-d3-v4/sunburst-tut01-d3-v4.js
+++ d3/sunburst-tut-d3-v4/sunburst-tut01-d3-v4.js
@@ -1,13 +1,19 @@
-var nodeData = {
-    "name": "TOPICS", "children": [{
-        "name": "Topic A",
-        "children": [{"name": "Sub A1", "size": 4}, {"name": "Sub A2", "size": 4}]
-    }, {
-        "name": "Topic B",
-        "children": [{"name": "Sub B1", "size": 3}, {"name": "Sub B2", "size": 3}, {
-            "name": "Sub B3", "size": 3}]
-    }, {
-        "name": "Topic C",
-        "children": [{"name": "Sub A1", "size": 4}, {"name": "Sub A2", "size": 4}]
-    }]
+var nodeData = { "name": "TOPICS", "children": [{
+    "name": "Topic A",
+    "children": [{"name": "Sub A1", "size": 4}, {"name": "Sub A2", "size": 4}]
+  }, {
+    "name": "Topic B",
+    "children": [{"name": "Sub B1", "size": 3}, {"name": "Sub B2", "size": 3},
+                 {"name": "Sub B3", "size": 3}]
+  }, {
+    "name": "Topic C",
+    "children": [{"name": "Sub A1", "size": 4}, {"name": "Sub A2", "size": 4}]
+  }]
 };
+
+// Initializes variables
+var vWidth = 500;  // <-- 1
+var vHeight = 500;
+var vRadius = Math.min(vWidth, vHeight) / 2;  // < -- 2
+var vColor = d3.scaleOrdinal(d3.schemeCategory20b);   // <-- 3
+
