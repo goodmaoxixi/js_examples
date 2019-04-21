367310d6e278fd921e9812cc730e9ed8874fc0dc
Guanglin Du <guanglin.du@gmail.com>
2019-04-21 10:39:17 +0800

    Add d3/sequences-sunburst v4 & v5

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,4 +1,6 @@
 /**
+ * Created on Apr 17, 2019, Thu
+ * 
  * An arrow function expression is a syntactically compact alternative to a
  * regular function expression, although without its own bindings to the this,
  * arguments, super, or new.target keywords. Arrow function expressions are ill
@@ -11,9 +13,12 @@
  * (param1, param2, …, paramN) => expression
  * // equivalent to: => { return expression; }
  *
- * Two factors influenced the introduction of arrow functions: shorter functions
- *  and no existence of this keyword.
+ * Two factors influenced the introduction of arrow functions: 1. shorter functions
+ *  and 2. no existence of this keyword.
  */
+ /* 0. Syntax */
+console.log("-- 0. Syntax");
+
 var materials = [
   'Hydrogen',
   'Helium',
@@ -24,19 +29,20 @@ var materials = [
 // Parentheses are optional when there's only one parameter name:
 // (singleParam) => { statements }
 // singleParam => { statements }
-console.log(materials.map(material => material.length));
+var results = materials.map(material => material.length);
+console.log("0.1: " + results);
 // expected output: Array [8, 6, 7, 9]
 
 // The equivalent
-var results = materials.map(function(material) {
+results = materials.map(function(material) {
   return material.length;
 });
-//console.log(results);
+console.log("0.2: " + results);
 
 /* Advanced syntaxSection */
 // Parenthesize the body of function to return an object literal expression:
 var val = params => ({ foo: bar })
-console.log(val);
+console.log(typeof(val) + "; " + val);
 
 // Rest parameters and default parameters are supported
 // (param1, param2, ...rest) => { statements }
@@ -44,48 +50,55 @@ console.log(val);
 
 // Destructuring within the parameter list is also supported
 var f = ([a, b] = [1, 2], {x: c} = {x: a + b}) => a + b + c;
-console.log(f()); // 6
+console.log("Destructuring in arrow function: " + f()); // 6
 
-/* 1. Shorter functions */
-console.log("-- 1. Shorter functions");
-var elements = [
-  'Hydrogen',
-  'Helium',
-  'Lithium',
-  'Beryllium'
-];
+// The traditional equivalent
+var f2 = function([a, b] = [1, 2], {x: c} = {x: a + b}) {
+    return a + b + c;
+};
+console.log("Traditional destructuring: " + f2()); // 6
+
+
+/** 1. Shorter functions */
+
+console.log("\n-- 1. Shorter functions");
+var elements = materials;
 
 results = elements.map(function(element) {
   return element.length;
 }); // this statement returns the array: [8, 6, 7, 9]
+console.log("1.1: " + results);
 
 // The regular function above can be written as the arrow function below
 results = elements.map((element) => {
   return element.length;
 }); // [8, 6, 7, 9]
+console.log("1.2: " + results);
 
 // When there is only one parameter, we can remove the surrounding parenthesies:
 results = elements.map(element => {
   return element.length;
 }); // [8, 6, 7, 9]
+console.log("1.3: " + results);
 
 // When the only statement in an arrow function is `return`, we can remove `return` and remove
 // the surrounding curly brackets
 results = elements.map(element => element.length); // [8, 6, 7, 9]
+console.log("1.4: " + results);
 
 // In this case, because we only need the length property, we can use destructuring parameter:
 // Notice that the string `"length"` corresponds to the property we want to get whereas the
 // obviously non-special `lengthFooBArX` is just the name of a variable which can be changed
 // to any valid variable name you want
 results = elements.map(({ "length": lengthFooBArX }) => lengthFooBArX); // [8, 6, 7, 9]
-console.log(results);
+console.log("1.5: " + results);
 
 // This destructuring parameter assignment can be written as seen below. However, note that there
 // is no specific `"length"` to select which property we want to get. Instead, the literal name
 // itself of the variable `length` is used as the property we want to retrieve from the object.
 elements.map(({ length }) => length); // [8, 6, 7, 9]
-console.log(results);
+console.log("1.6: " + results);
 
 
-/* 2. No separate this */
-console.log("-- 2. No separate this");
+/** 2. No separate this */
+console.log("\n-- 2. No separate this");
